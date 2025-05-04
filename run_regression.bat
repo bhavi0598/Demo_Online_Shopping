b74759f32d171885ed14c8d8799ea0c15fc137df
bhavi0598 <bhavishdama@gmail.com>
2025-05-04 23:02:46 +0530

    modified commands in bat file and added new pdf file contains project overview description

--- run_regression.bat
+++ run_regression.bat
@@ -1,8 +1,5 @@
-@echo off
-echo Running all tests in parallel batches...
-REM Run pytest with 3 parallel threads, generating a detailed HTML report
-pytest -v -s tests/ --html=reports/summary_report.html --self-contained-html -n 3
+rem pytest -v -s tests/ --html=reports/summary_report.html --self-contained-html -n 3
+pytest -v -s testCases/ --html=Reports/Summary_report/Summary_report.html --self-contained-html --capture=tee-sys
 
-REM Output a message after tests complete
-echo All tests executed and report generated in 'Reports/final_report.html'
-pause
+
+
